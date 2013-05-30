c78d4005543f3db14e64a323e9660051fbd69b31
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-05-30 13:26:59 +0200

    Integration of the webview with redbutton as default plugin

--- Awerem/src/com/github/awerem/aweremandroid/RemoteActivity.java
+++ Awerem/src/com/github/awerem/aweremandroid/RemoteActivity.java
@@ -1,23 +1,29 @@
 package com.github.awerem.aweremandroid;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
 
-public class RemoteActivity extends Activity {
+public class RemoteActivity extends Activity
+{
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote);
+        WebView remoteView = (WebView) findViewById(R.id.remote_view);
+        remoteView.loadUrl("http://192.168.1.14:34340/ui/redbutton");
+        remoteView.getSettings().setJavaScriptEnabled(true);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.remote, menu);
         return true;
     }
-    
 }
