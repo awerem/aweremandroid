d5f92f8c0ffa2bb2cfb79f1936d8f7466f9e2d0b
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-06-12 13:15:36 +0200

    progress is published  when an IP is found in discoverer

--- Awerem/src/com/github/awerem/aweremandroid/AsyncGetServers.java
+++ Awerem/src/com/github/awerem/aweremandroid/AsyncGetServers.java
@@ -1,19 +1,31 @@
 package com.github.awerem.aweremandroid;
 
+import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
-
-import com.github.awerem.aweremandroid.internet.ServerDiscoverer;
+import android.util.Base64;
+import android.util.Log;
 
 class AsyncGetServers extends
         AsyncTask<Void, InetAddress, ArrayList<InetAddress>>
 {
 
+    private static final int AWEREM_PORT = 34340;
+    private static final int TIMEOUT = 1000;
+    private static final String DEBUG_TAG = "AsyncGetServers";
     private WeakReference<Activity> ctx;
 
     public AsyncGetServers(Activity ctx)
@@ -24,9 +36,14 @@ class AsyncGetServers extends
     @Override
     protected ArrayList<InetAddress> doInBackground(Void... params)
     {
-        ServerDiscoverer discoverer = new ServerDiscoverer(ctx);
-        return discoverer.gatherIPs();
+        return gatherIPs();
 
+    }
+    
+    @Override
+    protected void onProgressUpdate(InetAddress... values)
+    {
+        super.onProgressUpdate(values);
     }
 
     @Override
@@ -40,5 +57,71 @@ class AsyncGetServers extends
             ctx.get().finish();
         }
         super.onPostExecute(result);
+    }
+
+    public InetAddress getBroadcastAddress() throws IOException
+    {
+        if (ctx.get() == null)
+            return null;
+        WifiManager wifi = (WifiManager) ctx.get().getSystemService(
+                Context.WIFI_SERVICE);
+        DhcpInfo dhcp = wifi.getDhcpInfo();
+        // handle null somehow
+
+        int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
+        byte[] quads = new byte[4];
+        for (int k = 0; k < 4; k++)
+            quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
+        return InetAddress.getByAddress(quads);
+    }
+
+    public ArrayList<InetAddress> gatherIPs()
+    {
+        MulticastSocket socket = null;
+        byte[] buf = new byte[128];
+        DatagramPacket answer = new DatagramPacket(buf, buf.length);
+        ArrayList<InetAddress> addresses = new ArrayList<InetAddress>();
+        try
+        {
+            socket = new MulticastSocket(AWEREM_PORT);
+            socket.setBroadcast(true);
+            socket.setSoTimeout(TIMEOUT);
+            byte[] rToken = new byte[128];
+            SecureRandom.getInstance("SHA1PRNG").nextBytes(rToken);
+            rToken = MessageDigest.getInstance("SHA1").digest(rToken);
+            String token = Base64.encodeToString(rToken, Base64.DEFAULT).trim();
+            String requete = "awerem\nping\n" + token + "\n";
+            DatagramPacket packet = new DatagramPacket(requete.getBytes(),
+                    requete.length(), getBroadcastAddress(), AWEREM_PORT);
+            socket.send(packet);
+
+            try
+            {
+                while (true)
+                {
+                    socket.receive(answer);
+                    Log.d("SOCKET", "Socket received");
+                    Log.d("SOCKET", new String(answer.getData(), "ascii"));
+                    String content = new String(answer.getData(), "ascii");
+                    String[] lines = content.split("\\r?\\n");
+                    if (lines[1].startsWith("pong") && lines[2].equals(token))
+                    {
+                        publishProgress(answer.getAddress());
+                        addresses.add(answer.getAddress());
+                    }
+                }
+            }
+            catch (SocketTimeoutException e)
+            {
+                Log.i(DEBUG_TAG, "socket closed");
+                socket.close();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        Log.v(DEBUG_TAG, addresses.toString());
+        return addresses;
     }
 }
