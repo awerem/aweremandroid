9f169422e188b1a45a2f48a7580476d518ac2f80
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-06-13 15:42:56 +0200

    Discovering is now triggered every 3s

--- Awerem/src/com/github/awerem/aweremandroid/PairingActivity.java
+++ Awerem/src/com/github/awerem/aweremandroid/PairingActivity.java
@@ -1,22 +1,48 @@
 package com.github.awerem.aweremandroid;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class PairingActivity extends Activity
 {
 
-   
+    private Timer discoveryTaskTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pairing);
-        AsyncGetServers async = new AsyncGetServers(this);
-        async.execute();
-        
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onStart();
+        discoveryTaskTimer = new Timer();
+        discoveryTaskTimer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run()
+            {
+                Log.i("PairingActivity" ,"Discovery task triggered");
+                AsyncGetServers async = new AsyncGetServers(
+                        PairingActivity.this);
+                async.execute();
+            }
+        }, 0, 3000);
+    }
+    
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        discoveryTaskTimer.cancel();
     }
 
     @Override
