ca1ef0ced9647db662398de2d53991660a37be0c
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-07-16 19:49:52 +0200

    Callbacks are now triggered when needed
    For now, only primitive types are supported

--- Awerem/src/com/github/awerem/aweremandroid/web/RemoteJSInterface.java
+++ Awerem/src/com/github/awerem/aweremandroid/web/RemoteJSInterface.java
@@ -50,18 +50,18 @@ public class RemoteJSInterface
     @JavascriptInterface
     public Object sendAction(int id, String method)
     {
-  		Log.i("JSInterface", "action sent " + method + " with id: " + id);
+        Log.i("JSInterface", "action sent " + method + " with id: " + id);
         method = moduleName + "." + method;
         Object ret = null;
         try
         {
-        	if(args.get(id) != null)
-        	{
-        		Object[] arguments = args.get(id).toArray();
-        		ret = this.proxy.callEx(method, arguments);
-        	}
-        	else
-        		ret = this.proxy.call(method);
+            if (args.get(id) != null)
+            {
+                Object[] arguments = args.get(id).toArray();
+                ret = this.proxy.callEx(method, arguments);
+            }
+            else
+                ret = this.proxy.call(method);
         }
         catch (Exception e)
         {
@@ -69,7 +69,7 @@ public class RemoteJSInterface
         }
         finally
         {
-        	args.delete(id);
+            args.delete(id);
         }
         return ret;
     }
@@ -77,38 +77,41 @@ public class RemoteJSInterface
     @JavascriptInterface
     public void sendActionAsync(int id, String method)
     {
-        new AsyncTask<Object, Void, String>() {
+        new AsyncTask<Object, Void, Object>() {
+            int id = -1;
 
             @Override
-            protected String doInBackground(Object... params)
+            protected Object doInBackground(Object... params)
             {
-            	int id = (Integer) params[0];
-            	String method = (String) params[1];
-                Log.d("JSInterface", "\"" + method + "\" " + "\""
-                        + id + "\"");
-                String result = String.valueOf(RemoteJSInterface.this.sendAction(id, method));
-                if (result == null)
-                {
-                	result = "";
-                }
+                id = (Integer) params[0];
+                String method = (String) params[1];
+                Log.d("JSInterface", "\"" + method + "\" " + "\"" + id + "\"");
+                Object result = RemoteJSInterface.this.sendAction(id, method);
                 return result;
             }
 
             @Override
-            protected void onPostExecute(final String result)
+            protected void onPostExecute(Object result)
             {
-               if(result != null)
-                {
-                    RemoteJSInterface.this.injectJS(result);
-                }
+                RemoteJSInterface.this.triggerCallback(id, result);
                 super.onPostExecute(result);
             }
         }.execute(id, method);
     }
 
-    protected void injectJS(String injection)
+    protected void triggerCallback(int id, Object result)
     {
-        webview.loadUrl("javascript:" + injection);
+        Log.i("JSInterface", "triggerCallback with id: " + id);
+        if (result != null)
+        {
+            String str_result = String.valueOf(result);
+            webview.loadUrl("javascript:awerem.triggerCallback(" + id + ","
+                    + str_result + ");");
+        }
+        else
+        {
+            webview.loadUrl("javascript:awerem.triggerCallback(" + id + ");");
+        }
     }
 
     @JavascriptInterface
@@ -135,40 +138,40 @@ public class RemoteJSInterface
         Log.i("RemoteJSInterface", "elapsed time for " + this.tickName + ": "
                 + (System.nanoTime() - this.tickStart) / 1000000 + "ms");
     }
-    
+
     @JavascriptInterface
     public void addString(int id, String obj)
     {
-    	Log.d("JSInterface", obj.toString());
-    	if(args.get(id) == null)
-    		args.append(id, new ArrayList<Object>());
-    	args.get(id).add(obj);
+        Log.d("JSInterface", obj.toString());
+        if (args.get(id) == null)
+            args.append(id, new ArrayList<Object>());
+        args.get(id).add(obj);
     }
-    
+
     @JavascriptInterface
     public void addInt(int id, long obj)
     {
-    	Log.d("JSInterface", String.valueOf(obj));
-    	if(args.get(id) == null)
-    		args.append(id, new ArrayList<Object>());
-    	args.get(id).add(obj);
+        Log.d("JSInterface", String.valueOf(obj));
+        if (args.get(id) == null)
+            args.append(id, new ArrayList<Object>());
+        args.get(id).add(obj);
     }
-    
+
     @JavascriptInterface
     public void addBool(int id, boolean obj)
     {
-    	Log.d("JSInterface", String.valueOf(obj));
-    	if(args.get(id) == null)
-    		args.append(id, new ArrayList<Object>());
-    	args.get(id).add(obj);
+        Log.d("JSInterface", String.valueOf(obj));
+        if (args.get(id) == null)
+            args.append(id, new ArrayList<Object>());
+        args.get(id).add(obj);
     }
-    
+
     @JavascriptInterface
     public void addFloat(int id, double obj)
     {
-    	Log.d("JSInterface", String.valueOf(obj));
-    	if(args.get(id) == null)
-    		args.append(id, new ArrayList<Object>());
-    	args.get(id).add(obj);
+        Log.d("JSInterface", String.valueOf(obj));
+        if (args.get(id) == null)
+            args.append(id, new ArrayList<Object>());
+        args.get(id).add(obj);
     }
 }
