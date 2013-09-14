256fcbcfe079ce0992ec50108874f248868e7462
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-09-14 18:57:05 +0200

    Open connection by touching item in computer list

--- Awerem/src/com/github/awerem/aweremandroid/PairingActivity.java
+++ Awerem/src/com/github/awerem/aweremandroid/PairingActivity.java
@@ -5,13 +5,16 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PairingActivity extends Activity
 {
@@ -29,6 +32,19 @@ public class PairingActivity extends Activity
         computerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1);
         computerList = (ListView) findViewById(R.id.available_computers);
         computerList.setAdapter(computerAdapter);
+        computerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position,
+                    long id)
+            {
+                TextView text = (TextView) view;
+                Intent openRemote = new Intent(PairingActivity.this, RemoteActivity.class);
+                openRemote.putExtra("ip", text.getText());
+                startActivity(openRemote);
+                finish();
+            }
+        });
         spinner = (ProgressBar) findViewById(R.id.pairing_spinner);
     }
 
